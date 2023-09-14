9f174d9e65675d023fb804297b3aab843519df24
Adrian <adriangonzalesval@gmail.com>
2023-09-14 16:03:03 -0500

    release: Version 2.0.9

--- src/main/java/io/github/_4drian3d/kickredirect/configuration/Configuration.java
+++ src/main/java/io/github/_4drian3d/kickredirect/configuration/Configuration.java
@@ -13,19 +13,28 @@ import java.util.List;
 @ConfigSerializable
 @SuppressWarnings({"CanBeFinal", "FieldMayBeFinal"})
 public final class Configuration implements Section {
-    @Comment("Sets the list of available servers to forward to the player\nDepending on the configuration of sendMode it will be sent to one server or another")
+    @Comment("""
+            Sets the list of available servers to forward to the player
+            Depending on the configuration of sendMode it will be sent to one server or another""")
     private List<String> serversToRedirect = List.of("lobby1", "lobby2");
 
     @Comment("Redirect the player if the expulsion message is null or empty")
     private boolean redirectOnNullMessage = true;
 
-    @Comment("Sets whether to perform whitelist or blacklist detection\nAvailable options:\nWHITELIST: It will check if the expulsion string contains any of this strings\nBLACKLIST: It will check if the expulsion string not contains any of this strings")
+    @Comment("""
+            Sets whether to perform whitelist or blacklist detectionAvailable options:
+            WHITELIST: It will check if the expulsion string contains any of this strings
+            BLACKLIST: It will check if the expulsion string not contains any of this strings""")
     private CheckMode checkMode = CheckMode.WHITELIST;
 
     @Comment("Set the messages to be checked by blacklist or whitelist in case they are present in the expulsion message")
     private List<String> messagesToCheck = List.of("kicked from server", "shutdown");
 
-    @Comment("Sets the sending mode\nAvailable options:\nTO_FIRST | It will send the player to the first available server configured in serversToRedirect\nTO_EMPTIEST_SERVER | Send the player to the emptiest server that is available according to the serversToRedirect configuration\nRANDOM | Send to a random server from the configured servers")
+    @Comment("""
+            Sets the sending modeAvailable options:
+            TO_FIRST | It will send the player to the first available server configured in serversToRedirect
+            TO_EMPTIEST_SERVER | Send the player to the emptiest server that is available according to the serversToRedirect configuration
+            RANDOM | Send to a random server from the configured servers""")
     private SendMode sendMode = SendMode.TO_FIRST;
 
     @Comment("Sets the limit of times the random server will be calculated to send in case the sending mode is RANDOM")
@@ -34,9 +43,10 @@ public final class Configuration implements Section {
     @Comment("Enables debug mode")
     private boolean debug = false;
 
-    @Comment("Sets the priority of the main KickRedirect listener."
-            +"\nThis may solve compatibility issues with other plugins."
-            +"\nAvailable Values: EARLY, FIRST, NORMAL, LATE")
+    @Comment("""
+            Sets the priority of the main KickRedirect listener.
+            This may solve compatibility issues with other plugins.
+            Available Values: EARLY, FIRST, NORMAL, LATE""")
     private PostOrder listenerPriority = PostOrder.EARLY;
 
     public List<String> getServersToRedirect(){
